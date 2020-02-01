de78a399f9724bdbf811193952c6a2222c0fd2a1
linjun <linjun@inke.cn>
2020-02-01 17:12:31 +0800

    first commit

---
+++ enode.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"linsul/etf"
+	"linsul/node"
+	"log"
+)
+
+var enableNode bool
+var nodeName string
+var nodeCookie string
+var nodePort int
+
+func init() {
+	flag.BoolVar(&enableNode, "node", false, "start erlang node")
+	flag.StringVar(&nodeName, "node-name", "", "name of erlang node")
+	flag.StringVar(&nodeCookie, "node-cookie", "", "cookie of erlang node")
+	flag.IntVar(&nodePort, "node-port", 5858, "port of erlang node")
+}
+
+func nodeEnabled() bool {
+	return enableNode
+}
+
+func runNode() (enode *node.Node) {
+	enode = node.NewNode(nodeName, nodeCookie)
+	err := enode.Publish(nodePort)
+	if err != nil {
+		log.Printf("Cannot publish: %s", err)
+		enode = nil
+	}
+	eSrv := new(eclusSrv)
+	enode.Spawn(eSrv)
+
+	eClos := func(terms etf.List) (r etf.Term) {
+		r = etf.Term(etf.Tuple{etf.Atom("enode"), len(terms)})
+		return
+	}
+
+	err = enode.RpcProvide("enode", "lambda", eClos)
+	if err != nil {
+		log.Printf("Cannot provide function to RPC: %s", err)
+	}
+
+	return
+}
---
+++ esrv.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"linsul/etf"
+	"linsul/node"
+	"log"
+)
+
+type eclusSrv struct {
+	node.GenServerImpl
+}
+
+func (es *eclusSrv) Init(args ...interface{}) {
+	log.Printf("ECLUS_SRV: Init: %#v", args)
+	es.Node.Register(etf.Atom("eclus"), es.Self)
+}
+
+func (es *eclusSrv) HandleCast(message *etf.Term) {
+	log.Printf("ECLUS_SRV: HandleCast: %#v", *message)
+}
+
+func (es *eclusSrv) HandleCall(message *etf.Term, from *etf.Tuple) (reply *etf.Term) {
+	log.Printf("ECLUS_SRV: HandleCall: %#v, From: %#v", *message, *from)
+	replyTerm := etf.Term(etf.Tuple{etf.Atom("ok"), etf.Atom("eclus_reply"), *message})
+	reply = &replyTerm
+	return
+}
+
+func (es *eclusSrv) HandleInfo(message *etf.Term) {
+	log.Printf("ECLUS_SRV: HandleInfo: %#v", *message)
+}
+
+func (es *eclusSrv) Terminate(reason interface{}) {
+	log.Printf("ECLUS_SRV: Terminate: %#v", reason.(int))
+}
